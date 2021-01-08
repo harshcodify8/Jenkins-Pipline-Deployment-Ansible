760e2c97ca63602e18fc79ad53d502f0dbf9aff0
icracked07 <42267256+icracked07@users.noreply.github.com>
2021-01-09 00:28:38 +0530

    Update HelloWorldController.java

--- src/main/java/com/wipro/app/controller/HelloWorldController.java
+++ src/main/java/com/wipro/app/controller/HelloWorldController.java
@@ -1,12 +1,11 @@
 package com.wipro.app.controller;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-	@RequestMapping(value="/")
+	@RequestMapping("/")
 	public String hello(){
 		return "Hello World!!";
 	}
